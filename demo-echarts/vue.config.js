85508d3a4b3eba8779616690a83a097834b69662
wanghai1 <2516649312@qq.com>
2020-07-05 01:15:12 +0800

    fead: 柱状图渐变色

--- demo-echarts/vue.config.js
+++ demo-echarts/vue.config.js
@@ -1,12 +1,17 @@
-module.exports ={
-    devServer :{
-        open:false,
-        port: 8081,
-        proxy :{
-            '/music': {
-                target: 'https://c.y.qq.com/splcloud/fcgi-bin', // 代理的基础路径
-            pathRewrite: {'^/music': ''}
-          }  
-        }
+// const path = require("path");
+
+module.exports = {
+  devServer: {
+    open: false,
+    port: 8081,
+    proxy: {
+      "/music": {
+        target: "https://c.y.qq.com/splcloud/fcgi-bin", // 代理的基础路径
+        pathRewrite: { "^/music": "" }
+      }
     }
-}
+    // resolve: {
+    //   "@": path.join(__dirname, "src")
+    // }
+  }
+};
